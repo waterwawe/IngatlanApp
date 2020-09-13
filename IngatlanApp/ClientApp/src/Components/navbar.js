52b51dfa43006f43fa8fef9e589fbfa2c4723a1a
WaterWawe <wawe.freestyle7@gmail.com>
2020-09-13 14:44:08 +0200

    Logo added

--- IngatlanApp/ClientApp/src/Components/navbar.js
+++ IngatlanApp/ClientApp/src/Components/navbar.js
@@ -1,38 +1,40 @@
-import React,{useState,useEffect} from 'react';
-import {Navbar,Nav,NavDropdown,DropdownButton,Button} from 'react-bootstrap';
-import {ApiCallMessage} from '../Api';
+import React, { useState, useEffect } from 'react';
+import { Navbar, Nav, NavDropdown, DropdownButton, Button, Image } from 'react-bootstrap';
+import Logo from './pics/logo.PNG';
 
-export default function MyNavbar({loggedinState, username}){
+export default function MyNavbar({ loggedinState, username }) {
 
   useEffect(() => {
-  }, [loggedinState,username])
+  }, [loggedinState, username])
 
-    return(
+  return (
     <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-    <Nav className="justify-content-flex-end">
-    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-    </Nav>
-    <Navbar.Collapse id="responsive-navbar-nav">
-    <Navbar.Brand href="/#/">LocalhoME</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="/#/">Home</Nav.Link>
-      <Nav.Link href="/#/search">Search properties</Nav.Link>
-    </Nav>
-    <Nav>
-        {loggedinState?
-        <DropdownButton variant="outline-info" drop="left" title={`Logged in as ${username}`}>
-          <NavDropdown.Item href="/#/messages">My messages</NavDropdown.Item>
-        <NavDropdown.Item href="/#/profile">Edit profile</NavDropdown.Item>
-        <NavDropdown.Item href="/#/manage">Manage my houses</NavDropdown.Item>
-        <NavDropdown.Item href="/#/newingatlan">Upload a new ad</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="/#/logout">Logout</NavDropdown.Item>
-        </DropdownButton> : 
-        <Button href ="/#/login"variant="outline-info">Login</Button>}
-      
-    </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-    );
+      <Nav className="justify-content-flex-end">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      </Nav>
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Brand href="/#/">
+          <Image className="brand-logo" src={Logo} alt="RealSTATE logo"/>
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/#/">Home</Nav.Link>
+          <Nav.Link href="/#/search">Search properties</Nav.Link>
+        </Nav>
+        <Nav>
+          {loggedinState ?
+            <DropdownButton variant="outline-info" drop="left" title={`Logged in as ${username}`}>
+              <NavDropdown.Item href="/#/messages">My messages</NavDropdown.Item>
+              <NavDropdown.Item href="/#/profile">Edit profile</NavDropdown.Item>
+              <NavDropdown.Item href="/#/manage">Manage my houses</NavDropdown.Item>
+              <NavDropdown.Item href="/#/newingatlan">Upload a new ad</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/#/logout">Logout</NavDropdown.Item>
+            </DropdownButton> :
+            <Button href="/#/login" variant="outline-info">Login</Button>}
+
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 
 }
