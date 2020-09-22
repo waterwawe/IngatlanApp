ac18903ef2108c8a13999e4627e5a8d37ff5ea19
WaterWawe <wawe.freestyle7@gmail.com>
2020-09-22 17:58:29 +0200

    map slightly works

--- IngatlanApp/ClientApp/src/Components/map.js
+++ IngatlanApp/ClientApp/src/Components/map.js
@@ -1,83 +1,82 @@
-import React, { Component } from 'react';
-import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
-import CurrentLocation from './location';
+import React, { useState } from 'react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow, CurrentLocation } from 'google-maps-react';
 
-export class MapContainer extends Component {
-  state = {
-    showingInfoWindow: false,  //Hides or the shows the infoWindow
-    activeMarker: {},          //Shows the active marker upon click
-    selectedPlace: {},          //Shows the infoWindow to the selected place upon a marker
-  };
+const mapStyles = {
+  width: '96%',
+  height: '94%'
+};
 
-  onDragEnd = (coord) => {
+export function MapContainer({updateLocation, center, google }) {
+  const [activeMarker, setActiveMarker] = useState();
+  const [showingInfoWindow, setShowingInfoWindow] = useState(false);
+
+  const onDragEnd = (coord) => {
     const { latLng } = coord;
     const lat = latLng.lat();
     const lng = latLng.lng();
-    this.props.updateLocation(lng, lat);
+    updateLocation(lng, lat);
   }
 
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
+  const onMarkerClick = (props, marker, e) => {
+    setActiveMarker(marker);
+    setShowingInfoWindow(true);
+  };
 
-  onClose = props => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      });
+  const onClose = props => {
+    if (showingInfoWindow) {
+      setActiveMarker(null);
+      setShowingInfoWindow(false);
     }
   };
 
-  render() {
-    return (
-      <>
-        {
-          this.props.center ?
-            <CurrentLocation
-              google={this.props.google}
-              initialcenter={this.props.center}>
-              <Marker onClick={this.onMarkerClick} position={this.props.center} draggable={true} name={'your estates location'} onDragend={(t, map, coord) => this.onDragEnd(coord)} />
-
-              <InfoWindow
-                marker={this.state.activeMarker}
-                visible={this.state.showingInfoWindow}
-                onClose={this.onClose}
-              >
-                <div>
-                  <h4>{this.state.selectedPlace.name}</h4>
-                </div>
-              </InfoWindow>
-            </CurrentLocation>
-            :
-            <CurrentLocation
-              centerAroundCurrentLocation
-              google={this.props.google}>
-                
-              <Marker onClick={this.onMarkerClick} name={'your estates location'} draggable onDragend={(t, map, coord) => this.onDragEnd(coord)} />
-              <InfoWindow
-                marker={this.state.activeMarker}
-                visible={this.state.showingInfoWindow}
-                onClose={this.onClose}
-              >
-                <div>
-                  <h4>{this.state.selectedPlace.name}</h4>
-                </div>
-              </InfoWindow>
-            </CurrentLocation>
-        }
-      </>
-    );
-  }
+  return (
+    <>
+      {center ? <Map
+        google={google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={center}>
+        <Marker
+          onClick={onMarkerClick}
+          name={"Your estate's location"}
+          draggable={true}
+          onDragend={(t, map, coord) => onDragEnd(coord)}
+        />
+        <InfoWindow
+          marker={activeMarker}
+          visible={showingInfoWindow}
+          onClose={onClose}
+        >
+          <div>
+            <h4>Your estate's location</h4>
+          </div>
+        </InfoWindow>
+      </Map> 
+      :
+      <CurrentLocation
+      centerAroundCurrentLocation
+      google={this.props.google}
+    >
+      <Marker onClick={this.onMarkerClick} name={"Your estate's location"} />
+      <InfoWindow
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}
+        onClose={this.onClose}
+      >
+        <div>
+          <h4>Your estate's location</h4>
+        </div>
+      </InfoWindow>
+    </CurrentLocation>
+    }
+    </>
+  );
 }
 
 export default GoogleApiWrapper(
   (props) => ({
     apiKey: "AIzaSyApJurg5jDh3TeApK0pe9zIDuKSfaf7p94",
+    center: props.center,
     updateLocation: props.updateLocation,
-    center: props.center
   }
   ))(MapContainer)
